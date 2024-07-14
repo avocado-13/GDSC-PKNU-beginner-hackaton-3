c90904370848183a314ba52c4454a9c7ffdc9d47
squareCaaat <140076739+squareCaaat@users.noreply.github.com>
2024-07-14 21:44:28 +0900

    init file

---
+++ src/main/java/com/example/gdscproject/GdscprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdscproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdscprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdscprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gdscproject/GdscprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdscproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
